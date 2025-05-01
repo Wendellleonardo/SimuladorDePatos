dba08bafb0c2cdf1abfac818a8c601b5c783ff57
Wendell <161265895+Wendellleonardo@users.noreply.github.com>
2025-05-01 17:50:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimuladorDePatos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MallardDuck mk = new MallardDuck();
+        mk.display();
+
+        RubberDuck borracha = new RubberDuck();
+        borracha.display();
+        borracha.Quack();
+
+        RedHeadDuck rh = new RedHeadDuck();
+        rh.display();
+        rh.Fly();
+        rh.Quack();
+
+        // Console.WriteLine("Hello, World!");
+    }
+}
